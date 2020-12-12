dea1c8018dc41763d239b5fd1228c82bd7cbd804
Kawshik Lodh <lodh.kawshik@gmail.com>
2020-12-12 22:56:50 +0530

    Added PubSub Model

--- force-app/main/default/lwc/objectDataComponent/objectDataComponent.js
+++ force-app/main/default/lwc/objectDataComponent/objectDataComponent.js
@@ -1,5 +1,24 @@
-import { api, LightningElement } from 'lwc';
+import { api, LightningElement,wire } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
 
+import pubsub from 'c/pubsub';
 export default class ObjectDataComponent extends LightningElement {
-    @api objectName;  
+    @api objectName = 'Customer Details';
+    @wire(CurrentPageReference) pageRef;
+
+    connectedCallback(){
+        this.register();
+    }
+
+    register(){
+        // console.log("Event Registered...");
+        pubsub.register('objectName', this.handleSub.bind(this));
+    }
+
+    handleSub(pubResponse){
+        // console.log(pubResponse);
+        this.objectName = pubResponse?pubResponse + ' Details':'Object Details';
+    }
+
+    
 }
--- force-app/main/default/lwc/sideBarComponent/sideBarComponent.js
+++ force-app/main/default/lwc/sideBarComponent/sideBarComponent.js
@@ -1,18 +1,34 @@
-import { LightningElement, track, wire } from 'lwc';
+import { LightningElement, track, wire, } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
 import getAllObjects from '@salesforce/apex/ResortAppClass.getAllObjects';
 import User_Id from '@salesforce/user/Id';
+import pubsub from 'c/pubsub';
 export default class SideBarComponent extends LightningElement {
     @track objects;
     @track error;
+    
+    @wire(CurrentPageReference) pageRef;
 
     @wire(getAllObjects,{userId: User_Id}) 
     wireObjects({error,data}){
         if(data){
             this.objects = data;
+            console.log(data);
             this.error = undefined;
         } else if(error){
             this.objects = undefined;
             this.error = error;
         }
+    }
+
+    handleClick(event){
+        var objectName = event.target.label;
+        console.log(objectName);
+        var payload = {
+            "Name":{},
+            
+        };
+        pubsub.fire('objectName', objectName);
+        console.log("Event Fired...");
     }
 }
